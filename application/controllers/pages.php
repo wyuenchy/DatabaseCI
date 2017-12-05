b6779b792c8573edfd33ef8ae84a9dfa985d41dd
Alex <wyuenchy@gmail.com>
2017-12-05 23:10:00 +0800

    changed structure

--- application/controllers/pages.php
+++ application/controllers/pages.php
@@ -5,16 +5,18 @@ class Pages extends CI_Controller{
         show_404();
       }
       $this->load->database();
-      $query="SELECT session_title,trainer_name,organizer_name,date,venue_name,time,no_of_tickets,level,session_ID "
+      $query="SELECT DISTINCT session_title,trainer_name,organizer_name,date,venue_name,DATE_FORMAT(start_time, '%H:%i')as'time',no_of_tickets,level,session_ID "
       ."FROM organizer"
       ." NATURAL JOIN session"
-      ." NATURAL JOIN sports_session"
       ." NATURAL JOIN trainer"
       ." NATURAL JOIN venue"
+      ." NATURAL JOIN session_organizer"
+      ." NATURAL JOIN session_trainer"
+      ." NATURAL JOIN session_venue"
+      ." NATURAL JOIN sport_cat"
       ." WHERE is_featured='1'";
   		$sql_query = $this->db->query($query);
   		$data["session"] = $sql_query;
-
       $data['title']=ucfirst($page);
       $this->load->view('/template/header');
       $this->load->view('/template/nav');
@@ -24,12 +26,15 @@ class Pages extends CI_Controller{
   }
   public function browse(){
     $this->load->database();
-    $query="SELECT session_title,trainer_name,organizer_name,date,venue_name,time,no_of_tickets,level,session_ID "
+    $query="SELECT DISTINCT session_title,trainer_name,organizer_name,date,venue_name,DATE_FORMAT(start_time, '%H:%i')as'time',no_of_tickets,level,session_ID "
     ."FROM organizer"
     ." NATURAL JOIN session"
-    ." NATURAL JOIN sports_session"
     ." NATURAL JOIN trainer"
-    ." NATURAL JOIN venue";
+    ." NATURAL JOIN venue"
+    ." NATURAL JOIN session_organizer"
+    ." NATURAL JOIN session_trainer"
+    ." NATURAL JOIN session_venue"
+    ." NATURAL JOIN sport_cat";
 		$sql_query = $this->db->query($query);
 		$data["session"] = $sql_query;
     $this->load->view('/template/header');
@@ -38,17 +43,31 @@ class Pages extends CI_Controller{
     $this->load->view('/template/footer');
   }
   public function details(){
-    $this->load->database();
-    $query="SELECT *"
-    // session_title,trainer_name,organizer_name,date,venue_name,time,no_of_tickets,level,session_ID
-    ."FROM organizer"
-    ." NATURAL JOIN session"
-    ." NATURAL JOIN sports_session"
-    ." NATURAL JOIN trainer"
-    ." NATURAL JOIN venue"
-    ." WHERE session_ID='"
-    .$this->uri->segment(3)
-    ."'";
+    // $this->load->database();
+    // $query="SELECT *"
+    // // session_title,trainer_name,organizer_name,date,venue_name,time,no_of_tickets,level,session_ID
+    // ."FROM organizer"
+    // ." NATURAL JOIN session"
+    // ." NATURAL JOIN trainer"
+    // ." NATURAL JOIN venue"
+    // ." NATURAL JOIN session_organizer"
+    // ." NATURAL JOIN session_trainer"
+    // ." NATURAL JOIN session_venue"
+    // ." NATURAL JOIN sport_cat";
+    // // .$this->uri->segment(3)
+    // // ."'";
+    // $sport_query = "SELECT DISTINCT category"
+    // "FROM organizer"
+    // "NATURAL JOIN session"
+    // "NATURAL JOIN trainer"
+    // "NATURAL JOIN venue"
+    // "NATURAL JOIN session_organizer"
+    // "NATURAL JOIN session_trainer"
+    // "NATURAL JOIN session_venue"
+    // "NATURAL JOIN sport_cat"
+    // "WHERE session_ID='"
+    //
+    // "'"
     $sql_query = $this->db->query($query);
     $data["session"] = $sql_query;
     $this->load->view('/template/header');
--- application/views/pages/details.php
+++ application/views/pages/details.php
@@ -1,7 +1,15 @@
+<h1>Details Of the session</h1>
+<div class='container'><br><br>
+  <table>
 <?php
 foreach ($session->result() as $row) {
-  echo$row->session_title."<br>";
-  echo$row->trainer_name."<br>";
-
+  echo "<tr><td>Session Name: </td><td>".$row->session_title."</td></tr>";
+  echo "<tr><td>Organizer Name: </td><td>".$row->organizer_name."</td></tr>";
+  echo "<tr><td>Session Date: </td><td>".$row->date."</td></tr>";
+  echo "<tr><td>Session Venue: </td><td>".$row->venue_name."</td></tr>";
+  echo "<tr><td>Session Start Time:&nbsp&nbsp</td><td>".$row->time."</td></tr>";
+  echo "<tr><td>Trainer Name:</td><td>".$row->trainer_name."</td></tr>";
 }
  ?>
+</table>
+</div>
