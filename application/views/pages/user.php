5eeda0a22636bd57a420c5edea9179fcb8fc8a8a
Alex <wyuenchy@gmail.com>
2017-12-06 23:13:45 +0800

    better

--- application/views/pages/user.php
+++ application/views/pages/user.php
@@ -6,14 +6,14 @@ header("location: login");}
     <div style="padding-left: 25%; padding-right: 25%;">
         <p style="font-size: 50px;">Welcome back, <?php echo $_SESSION['username']; ?></p>
         <form class="form-horizontal">
-            <button onclick="" class="btn btn-primary" style="width: 100%;">Check your sports session</button>
+            <a href="<?php base_url()?>/pages/Check" class="btn btn-primary" style="width: 100%;">Check your sports session</a></form>
             </br>
           </br>
-            <a href="edituser " class="btn btn-primary" style="width: 100%;">edit your information</a>
+            <a href="<?php base_url()?>/pages/edituser" class="btn btn-primary" style="width: 100%;">edit your information</a>
             </br>
           </br>
 
-          <a href="logout" class="btn btn-primary" style="width:100%;">log out</a>
+          <a href="<?php base_url()?>/pages/logout" class="btn btn-primary" style="width:100%;">log out</a>
 
 
 
